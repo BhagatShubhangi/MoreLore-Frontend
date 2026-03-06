304a9b7b524910838c30db6180f896ce86e28511
Shubhangi Bhagat <167609660+BhagatShubhangi@users.noreply.github.com>
2026-03-06 14:36:04 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "@/contexts/LanguageContext";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import Dashboard from "./pages/Dashboard";
+import AddStudent from "./pages/AddStudent";
+import StudentList from "./pages/StudentList";
+import StudentProfile from "./pages/StudentProfile";
+import EditStudent from "./pages/EditStudent";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <LanguageProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/add-student" element={<AddStudent />} />
+              <Route path="/students" element={<StudentList />} />
+              <Route path="/student/:id" element={<StudentProfile />} />
+              <Route path="/edit-student/:id" element={<EditStudent />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </LanguageProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
+
+export default App;
